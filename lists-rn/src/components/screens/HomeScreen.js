07dd54e8c5e1bb0091cf6de0b10a13916c3afd63
Adithya Kamath <adithyakamath96@gmail.com>
2021-06-17 21:19:02 +0530

    Playing with touchable opacity

--- lists-rn/src/components/screens/HomeScreen.js
+++ lists-rn/src/components/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -8,6 +8,9 @@ const HomeScreen = () => {
       <Button onPress={()=> console.log('Button Pressed')} 
         title="Components Screen"
       />
+      <TouchableOpacity onPress={()=> console.log('List TO Pressed')}>
+        <Text>Lists Screen</Text>
+      </TouchableOpacity>
     </View>
   );
 };
