ce75843155317d909e2cced7c41d8da41c13600d
Adithya Kamath <adithyakamath96@gmail.com>
2021-06-25 02:13:06 +0530

    Playing with flex

--- rn-styles-added/src/components/screens/BoxScreen.js
+++ rn-styles-added/src/components/screens/BoxScreen.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const BoxScreen = () => {
-    return (
-        <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box Screeb</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textOneStyle}>Box Screeb</Text>
+      <Text style={styles.textTwoStyle}>Box Screeb</Text>
 
-export default BoxScreen
+      <Text style={styles.textThreeeStyle}>Box Screeb</Text>
+    </View>
+  );
+};
+
+export default BoxScreen;
 
 const styles = StyleSheet.create({
-    viewStyle: {
-        borderWidth: 1,
-        borderColor: 'black'
-    },
-    textStyle:{
-        borderColor: 'red',
-        borderWidth: 1,
-        marginVertical: 20,
-        marginHorizontal: 20
-    }
-})
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    flexDirection: "row",
+  },
+  textOneStyle: {
+    borderColor: "red",
+    borderWidth: 1,
+    flex: 4,
+  },
+  textTwoStyle: {
+    borderColor: "red",
+    borderWidth: 1,
+    flex: 4,
+  },
+  textThreeeStyle: {
+    borderColor: "red",
+    borderWidth: 1,
+    flex: 2,
+  },
+});
